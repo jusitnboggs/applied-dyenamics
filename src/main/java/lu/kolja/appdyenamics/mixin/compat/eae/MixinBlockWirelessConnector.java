b7b1075ab0539649dd61d83dbe01d585861c99a7
Kolja <104643407+Thatguyello@users.noreply.github.com>
2025-10-16 15:14:35 +0200

    working textures and extendedae compat

--- src/main/java/lu/kolja/appdyenamics/mixin/compat/eae/MixinBlockWirelessConnector.java
+++ src/main/java/lu/kolja/appdyenamics/mixin/compat/eae/MixinBlockWirelessConnector.java
@@ -9,15 +9,21 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = BlockWirelessConnector.class, remap = false)
 public class MixinBlockWirelessConnector {
     @Mutable
-    @Shadow
     @Final
+    @Shadow(remap = false)
     private static IntegerProperty COLOR;
 
-    static {
+    @Inject(
+            method = "<clinit>",
+            at = @At("TAIL")
+    )
+    private static void init(CallbackInfo ci) {
         COLOR = IntegerProperty.create("color", 0, 34);
     }
 
--- src/main/java/lu/kolja/appdyenamics/mixin/compat/eae/MixinBlockWirelessHub.java
+++ src/main/java/lu/kolja/appdyenamics/mixin/compat/eae/MixinBlockWirelessHub.java
@@ -4,20 +4,28 @@ import com.glodblock.github.extendedae.common.blocks.BlockWirelessHub;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = BlockWirelessHub.class, remap = false)
+@Debug(export = true, print = true)
 public class MixinBlockWirelessHub {
     @Mutable
-    @Shadow
     @Final
+    @Shadow(remap = false)
     private static IntegerProperty COLOR;
 
-    static {
+    @Inject(
+            method = "<clinit>",
+            at = @At("TAIL")
+    )
+    private static void init(CallbackInfo ci) {
         COLOR = IntegerProperty.create("color", 0, 34);
     }
 
